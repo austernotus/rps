e98d8b6dde08a7801af8dfb36ad7fd17ad773fd0
austernotus <76696499+austernotus@users.noreply.github.com>
2024-05-16 21:40:02 +0100

    Added UI to RPS
    added buttons, paragraphs and being able to use the buttons to choose rock, paper or scissors

--- scripts.js
+++ scripts.js
@@ -2,6 +2,20 @@
 const rpsChoices = ['rock','paper','scissors']
 let humanScore = 0
 let computerScore = 0
+let rounds = 0
+let resetButton = document.getElementById("resetButton")
+
+const rockButton = document.getElementById("Rock")
+const paperButton = document.getElementById("Paper")
+const scissorsButton = document.getElementById("Scissors")
+const roundWinnerText = document.getElementById("roundWinner")
+const humanScoreText = document.getElementById("humanScore")
+const computerScoreText = document.getElementById("computerScore")
+const divOne = document.querySelector("div")
+
+rockButton.addEventListener("click", () => playRound("rock"))
+paperButton.addEventListener("click", () => playRound("paper"))
+scissorsButton.addEventListener("click", () => playRound("scissors"))
 
 function getComputerChoice(){
     choice = rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
@@ -19,41 +33,77 @@ function getHumanChoice(){
 
 function checkWinner(player,computer){
     if (player == "rock" && computer == "scissors"){
-        console.log("You win! Rock beats scissors.")
+        roundWinnerText.textContent = "You win! Rock beats scissors."
         humanScore++;
+        humanScoreText.textContent = "Your Score: " + humanScore;
     } else if ( player == "paper" && computer == "rock"){
-        console.log("You win! Paper beats rock.")
+        roundWinnerText.textContent = "You win! Paper beats rock."
         humanScore++;
+        humanScoreText.textContent = "Your Score: " + humanScore;
     } else if (player == "scissors" && computer == "paper"){
-        console.log("You win! Scissors beats paper.")
+        roundWinnerText.textContent = "You win! Scissors beats paper."
         humanScore++;
+        humanScoreText.textContent = "Your Score: " + humanScore;
     } else{
-        console.log("You lose! " + computer.charAt(0).toUpperCase() + computer.slice(1) + " beats " + player + "." )
+        roundWinnerText.textContent ="You lose! " + computer.charAt(0).toUpperCase() + computer.slice(1) + " beats " + player + "."
         computerScore++;
+        computerScoreText.textContent = "Computer Score: " + computerScore;
     }
+    checkGameOver();
 }
 
-function playRound(humanChoice,computerChoice){
-    computerChoice = getComputerChoice();
-    humanChoice = getHumanChoice();
+function playRound(humanChoice){
+    let computerChoice = getComputerChoice();
     if (computerChoice == humanChoice) {
-        console.log("Draw! You both chose " + humanChoice + ".");
+        roundWinnerText.textContent = "Draw! You both chose " + humanChoice + ".";
     }
     else {
         checkWinner(humanChoice, computerChoice);
     }
 }
 
-function playGame(){
-    let rounds = 0
-    while (rounds <5){
-        playRound();
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
-        rounds++;
+function checkGameOver(){
+    rounds++;
+    if (rounds === 5){
+        if (humanScore > computerScore){
+            alert("Game Over! You won! With " + humanScore + " wins vs their " + computerScore + " wins.")
+            humanScoreText.textContent = "👑 " + humanScoreText.textContent 
+        }
+        else if (computerScore > humanScore){
+            alert("Game Over! You lost! With " + humanScore + " wins vs their " + computerScore + " wins.")
+            computerScoreText.textContent = "👑 " + computerScoreText.textContent 
+        }
+        else{
+            alert("It's a draw! You both won " + computerScore + " times.")
+        }
+        
+        if (resetButton === null){
+            resetButton = document.createElement("button")
+            resetButton.textContent = "Reset"
+            resetButton.addEventListener("click", () => resetGame())
+            divOne.append(resetButton);
+        }
+        else{
+        }
+        rockButton.style.display = "none";
+        paperButton.style.display = "none";
+        scissorsButton.style.display = "none";
     }
-    console.log("Game Over! You won " + humanScore + " times out of 5!")
-
 }
 
-playGame()
+
+
+function resetGame(){
+    humanScore = 0
+    computerScore = 0
+    rounds = 0
+
+    rockButton.style.display = "inline-block";
+    paperButton.style.display = "inline-block";
+    scissorsButton.style.display = "inline-block";
+
+    humanScoreText.textContent = "Your Score: 0"
+    computerScoreText.textContent = "Your Score: 0"
+
+
+}
